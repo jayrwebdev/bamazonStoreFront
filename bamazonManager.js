3bf89a07d4cc1f41987fe681473a531300ef2654
Jahaan Rasheed <jahaanrasheed@Jahaans-MacBook-Pro.local>
2019-10-13 14:00:39 -0400

    view products for sale directory is operational

--- bamazonManager.js
+++ bamazonManager.js
@@ -18,5 +18,26 @@ inquirer.prompt([
     }
 
 ]).then(function(response){
-   console.log(response.directory)
+    var directory = response.directory
+   if (directory === "View Products for Sale") {
+        viewProduct()
+   } else if (directory === "View Low Inventory") {
+
+   } else if (directory === "Add To Inventory") {
+
+   } else if (directory === "Add New Product") {
+
+   }
 })
+
+function viewProduct() {
+    connection.query("SELECT * FROM Products", function(err,results){
+        if (err) {
+            console.log(err)
+        } else {
+            for (var i = 0; i < results.length; i++) {
+                console.log("ID:" + results[i].item_ID + " | Product: " + results[i].product_name + " | Department: " + results[i].department_name + " | Price: " + results[i].price + " | Stock: "+ results[i].stock_quantity)
+            }
+        }
+    })
+}
